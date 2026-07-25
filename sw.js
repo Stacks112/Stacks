049f6368e87dbb56c1a801b4c2b92f9b8d1ac7b5
Stacks112 <wnrakrhdn128@gmail.com>
2026-07-25 15:55:38 +0900

    refactor: index.html loads twc from assets/ (sw cache v18)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
      - everything else : cache first, refresh in the background
    Bump CACHE below to force everyone onto a clean cache. */
 
-const CACHE = "stacks-v17";
+const CACHE = "stacks-v18";
 
 const SHELL = [
   "./",
